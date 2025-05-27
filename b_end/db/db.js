592ab2601f16340a5784076310408f8281034882
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-05-27 13:41:19 +0100

    basic backend POC framework in place

---
+++ b_end/db/db.js
@@ -0,0 +1,28 @@
+const db = {
+    pets: [
+        {
+            id: 1,
+            name: 'Rex',
+            type: 'Dog',
+            age: 3,
+        breed: 'labrador',
+        },
+    
+        {
+            id: 2,
+            name: 'Fido',
+            type: 'Dog',
+            age: 1,
+            breed: 'poodle',
+        },
+        {
+            id: 3,
+            name: 'Mittens',
+            type: 'Cat',
+            age: 2,
+            breed: 'tabby',
+        },
+    ]
+}
+
+export default db
---
+++ b_end/pets/controllers/pets.controllers.js
@@ -0,0 +1,51 @@
+import { getItem, listItems, editItem, addItem, deleteItem } from '../../../b_end/pets/models/pets.models.js';
+
+export const getPet = (req, res) => {
+    try {
+        const resp = getItem(pareseInt(req.params.id))
+        res.status(200).json(resp)
+
+    }   catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export const listPets = (req, res) => {
+    try {
+        const resp = listItems()
+        res.status(200).json(resp)
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export const editPet = (req, res) => {
+    try {
+        const resp = addItem(req.body)
+        res.status(200).json(resp)
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export const addPet = (req, res) => {
+    try {
+        const resp = addItem(req.body)
+        res.status(201).json(resp)
+        
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export const deletePet = (req, res) => {
+    try {
+        const resp = deleteItem(parseInt(req.params.id))
+        res.status(200).json(resp)
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
