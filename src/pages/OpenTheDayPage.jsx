64dfec555f5716bada9311ee32e3326bb97e13d6
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-05-31 20:24:05 +0100

    hh

--- src/pages/OpenTheDayPage.jsx
+++ src/pages/OpenTheDayPage.jsx
@@ -6,7 +6,7 @@ export default function OpenTheDayPage() {
   return (
   <div className="card">
     <p className="text-center py-20">Open The Day Page - holding</p>
-    <Link to="/DashboardPage" className="btn-primary">Click to acknowledge handover notes</Link>
+    <Link to="/Dashboard" className="btn-primary">Click to acknowledge handover notes</Link>
     
     </div>
   )
