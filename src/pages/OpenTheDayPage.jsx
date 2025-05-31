76670c411c804604dd27cece1c3c4e6cae9e307a
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-05-31 20:27:48 +0100

    added dummy text

--- src/pages/OpenTheDayPage.jsx
+++ src/pages/OpenTheDayPage.jsx
@@ -6,6 +6,10 @@ export default function OpenTheDayPage() {
   return (
   <div className="card">
     <p className="text-center py-20">Open The Day Page - holding</p>
+    <p>Saturday 31st May 2025</p>
+    <p>Greg Walsh</p>
+    <p>Handover notes from yesterday:</p>
+    <p>"Quiet night, sent Bob home early. Managed to deep clean the cellar."</p>
     <Link to="/Dashboard" className="btn-primary">Click to acknowledge handover notes</Link>
     
     </div>
