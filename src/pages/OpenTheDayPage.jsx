755725c6a8dbf10e79b3e1d9ac1081abfd358f79
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-05-31 20:18:34 +0100

    as

--- src/pages/OpenTheDayPage.jsx
+++ src/pages/OpenTheDayPage.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
+
 import "../App.css"
 
 export default function OpenTheDayPage() {
   return (
   <div className="card">
     <p className="text-center py-20">Open The Day Page - holding</p>
-    <a href="/DashboardPage" className="btn-primary">Click to acknowledge handover notes</a>
+    <Link to="/DashboardPage" className="btn-primary">Click to acknowledge handover notes</Link>
     
     </div>
   )
