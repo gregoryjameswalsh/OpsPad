18c27a54326d79b0f91df1faab757197a8516305
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-06-03 10:48:19 +0100

    font checking

--- src/components/dashboard/DashboardHeader.jsx
+++ src/components/dashboard/DashboardHeader.jsx
@@ -3,7 +3,7 @@ import Badge from '../UI/Badge';
 export default function DashboardHeader() {
   return (
     <div className="flex flex-col md:flex-row items-start md:items-center justify-between bg-white p-4 rounded-lg shadow">
-      <h1 className="text-2xl font-bold">Today  </h1>
+      <h1 className="lato-black">Today  </h1>
       <div className="">
         <p>Monday, 2nd June, 2025</p>
         </div>
