181aa00c182bf1bcf3695288582207e98628e1c5
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-06-04 14:50:21 +0100

    added weather fetch api

--- src/components/app_core/AppHeader.jsx
+++ src/components/app_core/AppHeader.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import axios from 'axios'
 
 import '../../App.css'
 
 const AppHeader = () => {
+
+
+   useEffect(() => {
+    axios(`http://api.weatherapi.com/v1/current.json?key=bd87ea815f2449eb85c103309250406&q=London&aqi=no`)
+        .then(response => {
+            setData(response.data)
+        })
+        .then(data => {
+            setData(data);
+        })
+        .catch(error => {
+            console.error("Error fetching weather data: ", error);
+            setError(error);
+        })
+})
+
+
     return (
         <nav className="app-header">
             <div className="logo">
