22086f8366658e4034dc059b53f0f5ce4f75a68e
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-06-05 10:55:45 +0100

    start abstracting notes away to its own component

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -37,23 +37,7 @@ export default function DashboardPage() {
             {/* <TaskChecklistCard /> */}
           </div>
 
-          <div className="notes-section">
-            <h2>Ongoing Notes</h2>
-            <input className="note-input" type="text" placeholder="Add a note..." />
 
-            <div className="note">
-              <span className="tag incident">Incident</span>
-              <span className="note-time">7:32 AM</span>
-              <span className="note-text">Customer complaint about slow service</span>
-            </div>
-
-            <div className="note">
-              <span className="tag maintenance">Maintenance</span>
-              <span className="note-time">6:58 AM</span>
-              <span className="note-text">Leaking sink in kitchen</span>
-            </div>
-
-          </div>
 
           <div className="next-event">
             <h3>NEXT SCHEDULED EVENT</h3>
--- src/pages/ShiftNotesPage.jsx
+++ src/pages/ShiftNotesPage.jsx
@@ -1,3 +1,25 @@
+import '../App.css'
+
 export default function ShiftNotesPage() {
-  return <p className="text-center py-20">Shift Notes Page - holding</p>;
+  return (
+  
+          <div className="notes-section">
+            <h2>Ongoing Notes</h2>
+            <input className="note-input" type="text" placeholder="Add a note..." />
+
+            <div className="note">
+              <span className="tag incident">Incident</span>
+              <span className="note-time">7:32 AM</span>
+              <span className="note-text">Customer complaint about slow service</span>
+            </div>
+
+            <div className="note">
+              <span className="tag maintenance">Maintenance</span>
+              <span className="note-time">6:58 AM</span>
+              <span className="note-text">Leaking sink in kitchen</span>
+            </div>
+
+          </div>
+  
+  );
 }
