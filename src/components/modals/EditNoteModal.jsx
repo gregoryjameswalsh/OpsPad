46874a8f934e8bfffacb5aa2d85895414c4637ac
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-06-06 11:10:31 +0100

    r

--- src/components/modals/EditNoteModal.jsx
+++ src/components/modals/EditNoteModal.jsx
@@ -5,9 +5,22 @@ import '../../App.css'
 
 function EditNoteModal({ note, onSave, onDelete, onClose }) {
 
-    return (
-        <div>hello world</div>
-    )
+
+return (
+    <div className="modal-overlay">
+        <div className="modal-content">
+            <header className="modal-header">
+                <h2>Edit Note</h2>
+                <button className="close-button" onClick={onClose}>
+                    x
+                </button>
+            </header>
+        </div>
+    </div>
+
+
+
+)
 
 }
 
