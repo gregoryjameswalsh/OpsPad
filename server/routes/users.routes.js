e167d33893da3a3bf98de88b57b6678761012eec
gregoryjameswalsh <63927692+gregoryjameswalsh@users.noreply.github.com>
2025-06-19 15:19:59 +0100

    f

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -12,13 +12,9 @@ import {
 const router = express.Router();
 
 router.get('/', listUsers);
-
 router.get('/:id', getUser);
-
 router.put('/:id', editUser);
-
 router.post('/', addUser);
-
 router.delete('/:id', deleteUser);
 
 export default router;
